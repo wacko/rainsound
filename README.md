95afba87f78360c9c7de0f24bdc2f60bbd34c621
Joaquin Vicente <joakin@gmail.com>
2014-10-22 18:29:05 -0300

    original version

---
+++ README.md
@@ -0,0 +1,15 @@
+Rain sound
+==========
+
+A Chrome extension to improve productivity
+
+
+** How to install **
+
+You can install it from [Chrome web store](https://chrome.google.com/webstore/detail/rain-sound/boccolgmoknhiidceemmgkoaggficcpl)
+
+
+** Acknowledgement **
+
+Based on [Rainy Cafe](http://rainycafe.com/)
+Rain recording by [Matt Barnard](https://soundcloud.com/mattt)
---
+++ rain.js
@@ -0,0 +1,34 @@
+var status = 'none'
+var audio = document.createElement('audio')
+
+// Rain recording by Matt Barnard  / https://soundcloud.com/mattt
+audio.setAttribute('src', 'rain.mp3')
+audio.setAttribute('loop', 'true')
+
+  function audio_control() {
+    switch (status) {
+      case 'none':
+        audio.play()
+        audio.volume = 0.5
+        status = 'low'
+        break
+      case 'low':
+        audio.volume = 1
+        status = 'high'
+        break
+      case 'high':
+        audio.pause()
+        status = 'none'
+        break
+    }
+
+    chrome.browserAction.setIcon({
+      path: {
+        "19": status + "19px.png",
+        "38": status + "38px.png"
+      }
+    })
+
+  }
+
+chrome.browserAction.onClicked.addListener(audio_control)
